347f26b8eb69c944be31f1e8eda2a413a039f1d7
runandrerun <the.asantiagojr@gmail.com>
2018-07-27 12:07:01 -0400

    Done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,31 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = find_heroine
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.valid?
+      @heroine.save
+      redirect_to @heroine
+    else
+      render :new
+    end
+  end
+
+  private
+  def find_heroine
+    Heroine.find(params[:id])
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,22 @@
 class PowersController < ApplicationController
   def index
-    @powers = Power.all
+    # Allows for single search, but doesn't provide errors when it doesn't exists
+    # If it doesn't exist it reveals regular index page
+    power_par = params[:power]
+     if power = Power.where('name LIKE ?', "%#{power_par}%").exists?
+      power = Power.where('name LIKE ?', "%#{power_par}%")
+      redirect_to power_path(power.ids)
+    else
+      @powers = Power.all
+    end
+  end
+
+  def show
+    @power = find_power
+  end
+
+  private
+  def find_power
+    Power.find(params[:id])
   end
 end
